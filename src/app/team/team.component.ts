216b1057744974574fa95543e1ca5ce644e48909
Volodymyr Domin <108861073+mindotale@users.noreply.github.com>
2023-12-07 17:21:35 +0200

    feat: add components and a team member (#1)
    - Add main project components.
    - Add a team member's properties.

---
+++ src/app/team/team.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team',
+  templateUrl: './team.component.html',
+  styleUrls: ['./team.component.css']
+})
+export class TeamComponent {
+  teamMembers = [
+    {
+      name: 'Volodymyr Domin',
+      role: '.NET Developer',
+      photo: 'assets/team-photos/tm1.jpg',
+      color: '#cc00ff'
+    },
+    {
+      name: 'John Doe',
+      role: 'Developer',
+      photo: 'assets/team-photos/tm2.jpg',
+      color: '#ffcc00'
+    },
+    {
+      name: 'John Doe',
+      role: 'Developer',
+      photo: 'assets/team-photos/tm3.jpg',
+      color: '#ffcc00'
+    },
+    {
+      name: 'John Doe',
+      role: 'Developer',
+      photo: 'assets/team-photos/tm4.jpg',
+      color: '#ffcc00'
+    }
+  ];
+}
